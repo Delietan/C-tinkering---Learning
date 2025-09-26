80beb9dc4484a41a36699dec29ee155b31f940a2
Delietan <martinusnord@gmail.com>
2025-09-26 15:03:48 +0200

    gogo

---
+++ ARBEID/tinkering2.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+
+
+int main(){
+
+    int i;
+    printf("Laks\n");
+
+
+    // for loop
+    for (i = 0; i < 10; i++) {
+        printf("Sitron\n");
+        printf("%i stykkvis. I ganget med i %i \n", i, i*i);
+        }
+}
