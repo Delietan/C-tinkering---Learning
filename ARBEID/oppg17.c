34f292b4eb357f91f9dcb0672cd29e0753959943
Delietan <martinusnord@gmail.com>
2025-09-28 14:36:17 +0200

    yes

--- ARBEID/oppg17.c
+++ ARBEID/oppg17.c
@@ -1,50 +1,99 @@
 /**
- * Mitt svar på oppgave 17
+ * Min løsning på oppgave 17
  * @author Martinus Nordgård
- * @date 28 sept 2025
  * @file oppg17.c
+ * @date 28. sept
  */
 
 #include <stdio.h>
-#include <ctype.h>
 
-const int MINVERDI = 1;
-const int MAXVERDI = 10000;
+const int MAXLOPERE = 10;
+const int antallEgenskaper = 3;
 
 /**
- * Hovedprogrammet starter her
+ * Hovedprogrammer starter her
  */
 
- int main() {
+ int main(){
 
-    int intArray[5];
-    char charArray[10];
+    char vilDuKjore = 'a';
 
-    // for (int i = 0; i < 5; i++){ 
-    //     do {
-    //         printf("Skriv inn en verdi for skuffe nummer %i. Minst %i og maks %i.\n", i+1, MINVERDI, MAXVERDI);
-    //         scanf("%i", &intArray[i]);
-    //     } while (intArray[i] < MINVERDI || intArray[i] > MAXVERDI);
-    // };
-    // for (int i = 0; i < 5; i++){
-    //     printf("\nDin array er %i", intArray[i]);
-    // };
+    do {
+        int distanse, inputtaDistanse;
+        int antallFemhundremetre;
+        int inputtaLopere;
+        int i, j;
+        int inputtaMin, inputtaSek, inputtaCenti;
+        float poeng;
 
-
-    for (int i = 0; i < 5; i++){   
+        //innlesning av distanse
         do {
-            printf("Skriv inn en bokstav som skal i skuffe nr %i: ", i+1);
-            scanf(" %c", &charArray[i]);
-        } while (toupper(charArray[i]) < 65 || toupper(charArray[i]) > 65+26);
-    }
+            printf("Skriv ønsket distanse: ");
+            scanf("%i", &inputtaDistanse);
+        } while (inputtaDistanse != 500 && inputtaDistanse != 1500 &&
+                inputtaDistanse != 3000 && inputtaDistanse != 5000 );
+        distanse = inputtaDistanse;
 
-    for (int i = 0; i < 5; i++){      
-        charArray[i] = toupper(charArray[i]);
-    }
+        //poengberegning
+        antallFemhundremetre = inputtaDistanse / 500;
 
-    printf("Dine innleste bokstaver: ");
-    for (int i = 0; i < 5; i++){
-        printf("%c", charArray[i]);
-    }
+        printf("Distanse: %i", distanse);
+        printf("\nAntall 500-metere: %i", antallFemhundremetre);
 
+        //******************************************************************************
+
+        // innlesning av antall løpere
+        do {
+            printf("\nSkriv antall løpere: ");
+            scanf("%i", &inputtaLopere);
+        } while (inputtaLopere < 1 || inputtaLopere > MAXLOPERE);
+        int antallLopere = inputtaLopere;
+        int loperArray[antallLopere][antallEgenskaper];
+
+        //************************************************************************
+
+        //tildeling av tider til hver løper
+
+        for (i = 0; i < inputtaLopere; i++){
+            do {
+                printf("Skriv minutter brukt for løper nr.%i: ", i+1);
+                scanf("%i", &inputtaMin);
+            } while (60 < inputtaMin || inputtaMin < 0);
+
+            do {
+                printf("Skriv sekunder brukt for løper nr.%i: ", i+1);
+                scanf("%i", &inputtaSek);
+            } while (59 < inputtaSek || inputtaSek < 0);
+
+            do {
+                printf("Skriv hundredeler brukt for løper nr.%i: ", i+1);
+                scanf("%i", &inputtaCenti);
+            } while (99 < inputtaCenti || inputtaCenti < 0);
+
+            loperArray[i][0] = inputtaMin;
+            loperArray[i][1] = inputtaSek;
+            loperArray[i][2] = inputtaCenti;
+            
+            printf("\n");
+        };
+
+        //****************************************************************************
+
+        //poengtildeling
+        //utskrift av tider
+        for (i = 0; i < antallLopere; i++){
+            printf("Løper %i. fikk tid: ", i+1);
+            for (j = 0; j < antallEgenskaper; j++){
+                printf("%i ", loperArray[i][j]);
+            };
+            poeng = ((loperArray[i][0]*60) + loperArray[i][1] + (loperArray[i][2]/100)) / antallFemhundremetre;
+            printf("og poengscore: %.3f", poeng);
+            printf("\n");
+        };
+
+        printf("\nProgrammet avsluttes...");
+        printf("\nVaribler nullstilles...");
+        printf("\nVil du kjøre på nytt? (J/n) ");
+        scanf(" %c", &vilDuKjore);
+    } while (vilDuKjore == 'J' || vilDuKjore == 'j');
  }
